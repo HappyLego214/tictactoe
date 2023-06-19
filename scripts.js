cd2c9bcbc75f5dffc32aae99a0b30b90e9846bdb
HappyLego214 <anthony21josh@gmail.com>
2023-06-19 11:13:16 +0800

    adds length limit

--- scripts.js
+++ scripts.js
@@ -23,32 +23,86 @@ const startGame = (() => {
         return arr;
 
         }
-
     }
+
+    function _markGrid(location, arr, player, avail) {
+        if (location <= 3) {
+            let target = arr[1][location - 1];
+            let hit = player.playerMark[0];
+            if (hit.length >= 3) {
+                console.log('Slot Already Marked')
+               return avail = true;
+            } else {
+                console.log(target);
+
+                hit.push(target);
+    
+                console.log(hit);
+                return avail = false;
+            }
+
+        } else if (location <= 6 && location > 3) {
+            let target = arr[1][location - 4];
+            let hit = player.playerMark[1];
+            if (hit.length >= 3) {
+                console.log('Slot Already Marked')
+                return avail = true;
+            } else {
+                console.log(target);
+
+                hit.push(target);
+    
+                console.log(hit);
+                return avail = false;
+            }
+
+        } else if (location <= 9 && location > 6) {
+            let target = arr[1][location - 7];
+            let hit = player.playerMark[2];
+            if (hit.length >= 3) {
+                console.log('Slot Already Marked')
+                return avail = true;
+            } else {
+                console.log(target);
+
+                hit.push(target);
+    
+                console.log(hit);
+                return avail = false;
+            }
+
+        } else {
+            console.log('Error!');
+        }
+    }
+
 
     function _startRound(player1, player2) {
         let picker = true;
-        console.log(gameboard.arr);
+        let avail = true;
+        let arrRound = gameboard.arr;
         for(let i = 0; i < 9; i++) {
             if (picker == true) {
-                let name = player1.getName();
-                const location = prompt(`${name} | Place Your Marker: `);
-                console.log(`${name} | Marker Placed At ${location}`);
-                player1.markGrid(location);
+                do {
+                    let name = player1.getName();
+                    const location = prompt(`${name} | Place Your Marker: `);
+                    console.log(`${name} | Marker Placed At ${location}`);
+                    avail = _markGrid(location, arrRound, player1, avail);
+                    console.log(avail);
+                } while (avail == true);
                 picker = false;
             } else {
-                let name = player2.getName();
-                const location = prompt(`${name} | Place Your Marker: `);
-                console.log(`${name} | Marker Placed At ${location}`);
-                player2.markGrid(location);
+                do {
+                    let name = player2.getName();
+                    const location = prompt(`${name} | Place Your Marker: `);
+                    console.log(`${name} | Marker Placed At ${location}`);
+                    _markGrid(location, arrRound, player2);
+                    console.log(avail);
+                } while (avail == true);
                 picker = true;
             }
         }
     }
-
-    function _winConditions(arr) {
-
-    }   
 
     return {
         displayBoard: function() {
@@ -56,31 +110,21 @@ const startGame = (() => {
         },
 
         checkFunction: function() {
-            _startRound(player1, player2);
+            _startRound(player1, player2, gameboard.arr);
         }
     }
 
 })();
 
-const playerFactory = (name, score, arr) => {
-    let playerMark = [[],[],[]];
-    const markGrid = location => {
-        if (location <= 3) {
-            console.log(playerMark[0])
-        } else if (location <= 6) {
-            console.log(playerMark[1])
-        } else if (location <= 9) {
-            console.log(playerMark[2])
-        }
-    }
+const playerFactory = (name, score) => {
+    const playerMark = [[],[],[]];
     const getName = () => name;
     const getScore = () => score;
-    return {getName, getScore, markGrid, playerMark};
+    return {getName, getScore, playerMark};
 }
 
 const player1 = playerFactory('Player-1');
 const player2 = playerFactory('Player-2')
-
 
 // player1.getName();
 startGame.displayBoard();
